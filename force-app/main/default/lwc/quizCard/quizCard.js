5d06b949297591445e131c36b58a46f397334704
dalinwanglhtz <dalinwanglhtz@gmail.com>
2020-10-08 20:17:19 +1100

    A working flashcard set with pagination

--- force-app/main/default/lwc/quizCard/quizCard.js
+++ force-app/main/default/lwc/quizCard/quizCard.js
@@ -2,10 +2,13 @@ import { LightningElement, api } from 'lwc';
 
 export default class QuizCard extends LightningElement {
     @api cardData;
+    @api cardId;
     value = '';
 
     handleSelect(event) {
         this.value = event.detail.value;
+        const optionSelectEvent = new CustomEvent('selectedoption', {detail: {value: this.value, index: this.cardId}});
+        this.dispatchEvent(optionSelectEvent);
     }
 
 }
--- force-app/main/default/lwc/quizPage/quizPage.js
+++ force-app/main/default/lwc/quizPage/quizPage.js
@@ -5,5 +5,34 @@ export default class QuizPage extends LightningElement {
     value = '';
     startIndex = 0;
     endIndex = 1;
-    quizData = Data.quizData.slice(this.startIndex, this.endIndex);
+    allQuizes = this.updateIndex(Data.quizData.slice());
+    quizData = this.allQuizes.slice(this.startIndex, this.endIndex);
+
+    nextHandler() {
+        if(Data.quizData.length > this.endIndex) {
+            this.startIndex += 1;
+            this.endIndex += 1;
+            this.quizData = this.allQuizes.slice(this.startIndex, this.endIndex);
+        }
+    }
+
+    previousHandler() {
+        if(0 < this.startIndex) {
+            this.startIndex -= 1;
+            this.endIndex -= 1;
+            this.quizData = this.allQuizes.slice(this.startIndex, this.endIndex);
+        }
+    }
+
+    updateIndex(data) {
+        let newArray = data;
+        for(let i=0; i<newArray.length; i++) {
+            newArray[i].index = i;
+        }
+        return newArray;
+    }
+
+    handleSelectedOption(event) {
+        this.allQuizes[event.detail.index].selected = event.detail.value;
+    }
 }
