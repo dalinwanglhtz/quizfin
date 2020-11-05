1175f03ddeb0f506615a2b2db88e4730e0464568
dalinwanglhtz <dalinwanglhtz@gmail.com>
2020-11-05 22:30:24 +1100

    Adjusted quizHome with quizPage switch depending on username

--- force-app/main/default/lwc/quizHome/quizHome.js
+++ force-app/main/default/lwc/quizHome/quizHome.js
@@ -2,8 +2,20 @@ import { LightningElement } from 'lwc';
 
 export default class QuizHome extends LightningElement {
     userName;
+    entryAllowed;
 
     handleEnter(event) {
-        this.userName = this.template.querySelector('lightning-input').value.toUpperCase();
+        this.userName = this.template.querySelector('lightning-input').value;
+        // Based on username entered, check if this person exist, if so pull data
+        if(this.userName == 'Dale Wang') {
+            this.entryAllowed = true;
+        } else {
+            this.entryAllowed = false;
+        }
+        // If not, create new user data
+    }
+
+    connectedCallback() {
+
     }
 }
