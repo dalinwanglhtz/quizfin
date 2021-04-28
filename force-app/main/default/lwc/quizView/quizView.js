59cf57a7b24037415510bf31dd8fdcdac0f156fe
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-04-28 23:22:06 +1000

    test populate quizData array

--- force-app/main/default/lwc/quizView/quizView.js
+++ force-app/main/default/lwc/quizView/quizView.js
@@ -4,6 +4,14 @@ export default class QuizView extends LightningElement {
     quizData = [];
 
     connectedCallback() {
-        
+        let newQuestion = {
+            question: 'What is the best car',
+            answers: [
+                {label: 'honda', value:'honda'}
+            ],
+            correctAnswer: 'honda'
+        }
+
+        this.quizData.push(newQuestion);
     }
 }
