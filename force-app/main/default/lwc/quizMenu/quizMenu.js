e2265892fae882624bf8b32ff6c788775bceeb44
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-05-03 20:16:36 +1000

    update back home button with navigation mixin

--- force-app/main/default/lwc/quizMenu/quizMenu.js
+++ force-app/main/default/lwc/quizMenu/quizMenu.js
@@ -1,5 +1,14 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class QuizMenu extends LightningElement {
+export default class QuizMenu extends NavigationMixin(LightningElement) {
 
+    navigateToHomePage() {
+        this[NavigationMixin.Navigate]({
+            "type": "standard__namedPage",
+            "attributes": {
+                "pageName": 'home'
+            },
+        });
+    }
 }
