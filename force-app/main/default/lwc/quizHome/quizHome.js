9ffaf581cd5c82277bf31e1f45ad9d84ec95a615
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-05-03 17:30:36 +1000

    replace anchor with navigation mixin for page navigation

--- force-app/main/default/lwc/quizHome/quizHome.js
+++ force-app/main/default/lwc/quizHome/quizHome.js
@@ -1,5 +1,14 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class QuizHome extends LightningElement {
+export default class QuizHome extends NavigationMixin(LightningElement) {
 
+    navigateToAttendQuiz(event) {
+        this[NavigationMixin.Navigate]({
+            "type": "standard__webPage",
+            "attributes": {
+                "url": "/"+event.target.name
+            },
+        });
+    }
 }
