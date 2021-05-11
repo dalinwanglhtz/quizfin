cf6242ee2f3bbe6ad2d0d5920638d270443cb3e3
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-05-11 11:43:50 +1000

    remove unnecessary call to get default owner user id and add logic to close action modal after finish updating owner

--- force-app/main/default/lwc/quizUpdateOwner/quizUpdateOwner.js
+++ force-app/main/default/lwc/quizUpdateOwner/quizUpdateOwner.js
@@ -1,44 +1,22 @@
-import { LightningElement, wire, track, api } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { getRecord } from 'lightning/uiRecordApi';
 import { updateRecord } from 'lightning/uiRecordApi';
 import OWNER_ID_FIELD from '@salesforce/schema/Quiz__c.OwnerId';
 import QUIZ_ID_FIELD from '@salesforce/schema/Quiz__c.Id';
-import USER_ID_FIELD from '@salesforce/schema/User.Id';
 
 export default class QuizUpdateOwner extends LightningElement {
     @track error;
-    @track defaultUserId;
     @api recordId;
-
-    @wire(getRecord, {
-        recordId: '0052w000006t99qAAA',
-        fields: [USER_ID_FIELD]
-    })
-    getDefaultUser({
-        error,
-        data
-    }) {
-        if(data) {
-            console.log('Data: ', data);
-            this.defaultUserId = data.fields.Id.value;
-            this.updateQuiz();
-        } else if(error) {
-            this.error = error;
-        }
-    }
+    defaultUserId = '0052w000006t99qAAA';
 
     updateQuiz() {
-        console.log('User to be updated: ', this.defaultUserId);
         const fields = {};
-       // fields[QUIZ_ID_FIELD.fieldApiName] = this.recordId;
-        fields[QUIZ_ID_FIELD.fieldApiName] = 'a012w00000jpZxiAAE';
+        fields[QUIZ_ID_FIELD.fieldApiName] = this.recordId;
         fields[OWNER_ID_FIELD.fieldApiName] = this.defaultUserId;
         const recordInput = { fields };
 
         updateRecord(recordInput)
             .then(() => {
-                console.log('Sucessful');
                 this.dispatchEvent(
                     new ShowToastEvent({
                         title: 'Success',
@@ -48,7 +26,6 @@ export default class QuizUpdateOwner extends LightningElement {
                 );
             })
             .catch(error => {
-                console.log('Error: ', error);
                 this.dispatchEvent(
                     new ShowToastEvent({
                         title: 'Error updating record',
@@ -57,5 +34,10 @@ export default class QuizUpdateOwner extends LightningElement {
                     })
                 );
             });
+    }
+
+    connectedCallback() {
+        this.updateQuiz();
+        this.dispatchEvent(new CustomEvent('closeQuickAction'));
     }
 }
