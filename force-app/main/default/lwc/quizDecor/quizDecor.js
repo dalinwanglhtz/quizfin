c9aa6aa2b169a77885e3071f39ecf2203cb8c3d9
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-05-17 17:21:04 +1000

    add application logo

---
+++ force-app/main/default/lwc/quizDecor/quizDecor.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import QUIZFIN_LOGO from '@salesforce/resourceUrl/quizfin_logo';
+
+export default class QuizDecor extends LightningElement {
+    quizFinLogo = QUIZFIN_LOGO;
+}
