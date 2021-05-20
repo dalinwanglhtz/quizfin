62eae0bf3668b8092179ebf129243bf321b5f331
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-05-20 22:16:49 +1000

    update help page with additional info

--- force-app/main/default/lwc/quizHelp/quizHelp.js
+++ force-app/main/default/lwc/quizHelp/quizHelp.js
@@ -4,4 +4,6 @@ import QUIZ_DECOR from '@salesforce/resourceUrl/quiz_decor';
 export default class QuizHelp extends LightningElement {
     quizHelpImg1 = QUIZ_DECOR + '/quiz_help_img_1.png';
     quizHelpImg2 = QUIZ_DECOR + '/quiz_help_img_2.png';
+    quizHelpImg3 = QUIZ_DECOR + '/quiz_help_img_3.png';
+    quizHelpImg4 = QUIZ_DECOR + '/quiz_help_img_4.png';
 }
