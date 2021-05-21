f009bffd899e0838b8ae4fe185f301f2bfd6083c
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-05-21 15:37:01 +1000

    add help page link to site

--- force-app/main/default/lwc/quizDecor/quizDecor.js
+++ force-app/main/default/lwc/quizDecor/quizDecor.js
@@ -1,6 +1,16 @@
 import { LightningElement } from 'lwc';
 import QUIZ_DECOR from '@salesforce/resourceUrl/quiz_decor';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class QuizDecor extends LightningElement {
+export default class QuizDecor extends NavigationMixin(LightningElement) {
     quizFinLogo = QUIZ_DECOR+'/quizfin_logo.png';
+
+    navigateToHelpPage() {
+        this[NavigationMixin.Navigate]({
+            "type":"standard__namedPage",
+            "attributes": {
+                "pageName": "help"
+            },
+        });
+    }
 }
