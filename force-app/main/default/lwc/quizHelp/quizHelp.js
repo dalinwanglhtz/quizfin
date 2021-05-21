0ee0dfec534e224110de370dee84a7d75c983d15
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-05-21 13:17:57 +1000

    complete draft help page

--- force-app/main/default/lwc/quizHelp/quizHelp.js
+++ force-app/main/default/lwc/quizHelp/quizHelp.js
@@ -6,4 +6,14 @@ export default class QuizHelp extends LightningElement {
     quizHelpImg2 = QUIZ_DECOR + '/quiz_help_img_2.png';
     quizHelpImg3 = QUIZ_DECOR + '/quiz_help_img_3.png';
     quizHelpImg4 = QUIZ_DECOR + '/quiz_help_img_4.png';
+    quizHelpImg5 = QUIZ_DECOR + '/quiz_help_img_5.png';
+    quizHelpImg6 = QUIZ_DECOR + '/quiz_help_img_6.png';
+    quizHelpImg7 = QUIZ_DECOR + '/quiz_help_img_7.png';
+    quizHelpImg8 = QUIZ_DECOR + '/quiz_help_img_8.png';
+    quizHelpImg9 = QUIZ_DECOR + '/quiz_help_img_9.png';
+    quizHelpImg10 = QUIZ_DECOR + '/quiz_help_img_10.png';
+    quizHelpImg11 = QUIZ_DECOR + '/quiz_help_img_11.png';
+    quizHelpImg12 = QUIZ_DECOR + '/quiz_help_img_12.png';
+    quizHelpImg13 = QUIZ_DECOR + '/quiz_help_img_13.png';
+    quizHelpImg14 = QUIZ_DECOR + '/quiz_help_img_14.png';
 }
